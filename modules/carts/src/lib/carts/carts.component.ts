bd27ee49aa494077aea488388b0c8af5d621294f
Rafael Ferreira <rafaelblink@gmail.com>
2024-05-31 18:46:00 -0300

    feat: implements the order, produts modules

---
+++ modules/carts/src/lib/carts/carts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-carts',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carts.component.html',
+  styleUrls: ['./carts.component.css'],
+})
+export class CartsComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductsComponent } from '@module/products';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, ProductsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
